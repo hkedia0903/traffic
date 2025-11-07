497b380e01186a79c9969f0ca0dd669851023ddf
Hirsh Kedia <hkedia@calpoly.edu>
2025-11-07 13:43:11 -0800

    repush again

--- traffic.py
+++ traffic.py
@@ -12,7 +12,7 @@ warnings.filterwarnings("ignore")
 
 
 dt_pickle = open('traffic.pickle', 'rb') 
-reg = pickle.load(dt_pickle) 
+clf = pickle.load(dt_pickle) 
 dt_pickle.close()
 
 st.title('Traffic Volume Predictor') 
